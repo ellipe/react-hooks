020d0b4d5e1de0b4b4e0a9b6da223b75a46cd1ad
Luis Bertel <ellipe@gmail.com>
2021-01-02 13:00:45 -0500

    useState and useEffect.

---
+++ src/components/Characters.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+
+const Characters = () => {
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.results)
+        setCharacters(data.results)
+      })
+  }, [])
+
+  return (
+    <div className='characters'>
+      {characters.map((character) => (
+        <div className='character'>
+          <div className='character__image'>
+            <img src={character.image} alt={character.name} />
+          </div>
+          <div className='character__details'>
+            <h2 className='character__name' key={character.id}>
+              {character.name}
+            </h2>
+            <div className='character__species'>
+              Species: <em>{character.species}</em>
+            </div>
+            <div className='character__status'>
+              Status: <em>{character.status}</em>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Characters
